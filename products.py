7c8e35cc4b238abe7290f3e4fa1ac3fb2bb396af
sfangfang <98867639+sfangfang@users.noreply.github.com>
2023-06-14 23:21:36 +0800

    add check file

--- products.py
+++ products.py
@@ -4,27 +4,26 @@ Spyder Editor
 
 This is a temporary script file.
 """
-#讀取檔案
-products = []
-with open('products.txt','r', encoding='utf-8') as f:
-    for line in f:
-        if '商品,價格' in line:
-            continue
-        name, price = line.strip('\n').split(',')
-        products.append([name, price])
+import os
 
-# products = []
+products = []
+if os.path.isfile('path.csv'):
+    print('yeh')
+    #讀取檔案
+    with open('products.csv','r', encoding='utf-8') as f:
+        for line in f:
+            if '商品,價格' in line:
+                continue
+            name, price = line.strip('\n').split(',')
+            products.append([name, price])
+else:
+    print('No')
+
 while True:
     name = input('請輸入商品名稱: ')
     if name == 'q':
         break
     price = input('請輸入價格: ')
-# =============================================================================
-#     p = []
-#     p.append(name)
-#     p.append(price)
-#     products.append(p)
-# =============================================================================
     products.append([name, price])
 
 for p in products:
