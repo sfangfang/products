f0ffec4db64912bcbf2168cb71322d491fffb0fa
sfangfang <98867639+sfangfang@users.noreply.github.com>
2023-06-14 23:02:39 +0800

    rename

--- products.py
+++ products.py
@@ -4,7 +4,14 @@ Spyder Editor
 
 This is a temporary script file.
 """
+#讀取檔案
 products = []
+with open('products.txt','r', encoding='utf-8') as f:
+    for line in f:
+        name, price = line.strip('\n').split(',')
+        products.append([name, price])
+
+# products = []
 while True:
     name = input('請輸入商品名稱: ')
     if name == 'q':
@@ -20,6 +27,6 @@ while True:
 
 for p in products:
     print(p[0], '的價格是', p[1])
-with open('products.txt', 'w') as f:
+with open('products.txt', 'w', encoding='utf-8') as f:
     for p in products:
         f.write(p[0] + ',' + p[1] + '\n')
